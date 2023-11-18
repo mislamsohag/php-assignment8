bf80582eb74bcfa695ff0165eac79c905d05c557
mislamsohag <mislamsohag@gmail.com>
2023-11-18 12:21:53 +0600

    Code Update

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 
 Route::get('/profile/{id}',[ProfileController::class,'index'])->whereNumber("id");
